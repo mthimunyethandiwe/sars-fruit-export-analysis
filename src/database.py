1a34823a3c50f6c27735154bf1996c4cbb9323a6
thmthcpt025 <thmthcpt025@student.wethinkcode.co.za>
2026-08-16 04:43:57 +0200

    Add sqlite,raw_exports table and load all csvs

--- src/database.py
+++ src/database.py
@@ -1,5 +1,7 @@
 import sqlite3
 from src.config import DATA_DIR
+from src.config import RAW_DATA_DIR
+import pandas as pd
 
 DB_PATH = DATA_DIR / "exports.db"
 
@@ -16,18 +18,43 @@ CREATE TABLE IF NOT EXISTS raw_exports (
 );
 """
 
-def creat_connection():
+
+def create_connection():
     conn = sqlite3.connect(DB_PATH)
     return conn
 
+
 def create_table():
-    conn = creat_connection()
+    conn = create_connection()
     cursor = conn.cursor()
     cursor.execute(CREATE_TABLE_SQL)
     conn.commit()
     conn.close()
 
 
+def load_csv_to_db(csv_path):
+    df = pd.read_csv(csv_path)
+    df["Tariff"] = df["Tariff"].astype(str).str.zfill(8)
+    df["YearMonth"] = df["YearMonth"].astype(str)
+
+    conn = create_connection()
+    df.to_sql("raw_exports", conn, if_exists="append", index=False)
+    conn.close()
+
+    print(f"Loaded {len(df)} rows from {csv_path.name}")
+
+def load_all_csvs():
+    csv_files = RAW_DATA_DIR.glob("*.csv")
+    for csv_path in csv_files:
+        load_csv_to_db(csv_path)
+
+
+
+
+    
+
 if __name__ == "__main__":
     create_table()
-    print("Table created successfully")    
+    load_all_csvs()
+            
+    print("Table created and loaded successfully")
